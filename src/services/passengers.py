28d96f9615ed42d8e7454c9f69211c5705784244
Victor Huang <huang.victor.mail@gmail.com>
2025-01-06 17:04:06 +0100

    feat : get_country_totals service

--- src/services/passengers.py
+++ src/services/passengers.py
@@ -63,3 +63,35 @@ class PassengersService:
                 "error": "Erreur lors de la lecture du fichier",
                 "details": str(error)
             }), HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def get_country_totals(self, iso3):
+        """
+        Retourne le total des passagers domestiques et internationaux pour un pays
+        """
+        try:
+            df = pd.read_csv(self.file_path)
+            country_data = df[df['ISO3'] == iso3.upper()]
+
+            if country_data.empty:
+                return jsonify({
+                    "error": "Pays non trouvé"
+                }), HTTPStatus.NOT_FOUND
+
+            domestic_total = (
+                country_data[country_data['Domestic'].notna()]['Domestic'] * 1000
+            ).sum()
+            international_total = (
+                country_data[country_data['International'].notna()]['International'] * 1000
+            ).sum()
+
+            return jsonify({
+                "country": iso3.upper(),
+                "domestic_total":domestic_total,
+                "international_total": international_total
+            }), HTTPStatus.OK
+
+        except ImportError as error:
+            return jsonify({
+                "error": "Erreur lors de la lecture du fichier",
+                "details": str(error)
+            }), HTTPStatus.INTERNAL_SERVER_ERROR
