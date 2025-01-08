5b95262921459632a5524fa50bded0b0816b4e86
vitolinho <93949338+vitolinho@users.noreply.github.com>
2025-01-08 16:59:44 +0100

    feat : implement bar chart & nb passengers by holiday types

---
+++ client/pages/holidays-type-by-passengers.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+    const nuxtApp = useNuxtApp()
+
+    const { data: holidaysTypeByPassengers } = await useAsyncData(
+        `holidays-type-by-passengers`,
+        () => $fetch('/api/holidays-and-passengers/holidays-type-by-passengers'), {
+            getCachedData: key => nuxtApp?.payload?.data[key] || null
+        }
+    )
+
+    const holidaysTypeByPassengersData = Object.values(holidaysTypeByPassengers.value)
+    
+    const holidaysTypeByPassengersLabels = Object.keys(holidaysTypeByPassengers.value)
+
+</script>
+
+<template>
+    <BarChart
+        :data="holidaysTypeByPassengersData"
+        :labels="holidaysTypeByPassengersLabels"
+    />
+</template>
