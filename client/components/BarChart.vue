b6d4d9b71a7997226648bd9e87b1b436f1de15ed
vitolinho <93949338+vitolinho@users.noreply.github.com>
2025-01-09 10:02:38 +0100

    feat : implement multi stack bar graph for holidays and passengers number

--- client/components/BarChart.vue
+++ client/components/BarChart.vue
@@ -5,59 +5,95 @@
     const props = defineProps({
         labels: {
             type: Array as () => string[],
-            default: []
+            default: () => []
         },
 
         data: {
             type: Array as () => number[],
-            default: []
+            default: () => []
         },
 
         colorChart: {
             type: Array as () => string[],
             default: () => ["#C5D1EB", "#92AFD7", "#5A7684", "#395B50", "#D8DCFF", "#AEADF0", "#C38D94", "#A76571", "#565676"]
+        },
+
+        stackedBars: {
+            type: Boolean,
+            default: false
+        },
+
+        datasetsLabel: {
+            type: Array as () => string[],
+            default: () => []
+        },
+
+        grid: {
+            type: Boolean,
+            default: false
         }
     })
 
-    const chartData = {
-        labels: props.labels,
-        datasets: [{
-            label: 'Nombre de passagers',
+    const datasets = props.stackedBars
+        ? props.data.map((dataset, index) => ({
+            label: props.datasetsLabel[index],
+            data: dataset,
+            backgroundColor: props.colorChart[index % props.colorChart.length]
+        }))
+        : [{
+            label: props.datasetsLabel[0],
             data: props.data,
             backgroundColor: props.colorChart
         }]
+
+
+    const chartData = {
+        labels: props.labels,
+        datasets: datasets
     }
 
-    const chartRef = ref<any>(null)
+    const chartRef = ref<HTMLCanvasElement | null>(null)
 
     onMounted(() => {
         const ctx = chartRef.value.getContext('2d')
-        new Chart(ctx, {
-            type: 'bar',
-            data: chartData,
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        ticks: {
-                            callback: function(value) {
-                                return value.toLocaleString()
+        if (ctx) {
+            new Chart(ctx, {
+                type: 'bar',
+                data: chartData,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    scales: {
+                        x: {
+                            stacked: props.stackedBars,
+                            grid: {
+                                display: props.grid
+                            }
+                        },
+                        y: {
+                            stacked: props.stackedBars,
+                            beginAtZero: true,
+                            ticks: {
+                                callback: function(value) {
+                                    return value.toLocaleString()
+                                }
+                            },
+                            grid: {
+                                display: props.grid
                             }
                         }
-                    }
-                },
-                plugins: {
-                    legend: {
-                        display: false
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
                     }
                 }
-            }
-        })
+            })
+        }
     })
 </script>
 
 <template>
-    <canvas ref="chartRef" class="h-full" />
+    <canvas ref="chartRef" />
 </template>
